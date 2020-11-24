f785e1e3fb669ad00a84a3c9f85ed0891a2d3f85
stevistevanusgabriel <s21810544@student.unklab.ac.id>
2020-11-24 10:44:49 +0800

    besic javascript

---
+++ besicjavascript.js
@@ -0,0 +1,49 @@
+console.log("hallo javascript");
+
+// cara penulisan variabel
+//let & const
+//let bisa di ganti-ganti
+// let umur = 20;
+// umur = 21;
+// let nilai = 3.14;
+// console.log(nilai);
+// let masihBerkuliah = true;
+// console.log(masihBerkuliah);
+// let nama = "stevi stevanus gabriel";
+// console.log(umur);
+// console.log(nama);
+// let addres;
+// console.log(addres);
+// //const sama dengan nilai tetep
+// const tahunLahir = 2000;
+// console.log(tahunLahir);
+
+// oprator
+let angka1 =10;
+let angka2 =40;
+let hasil = angka1 + angka2;
+console.log(hasil);
+
+let umur =20;
+let namaDepan = "stevi";
+let namaTengah = "stevanus";
+let namaBelakang = "gabriel";
+console.log("nama saya " + namaDepan+" "+namaTengah+" "+ namaBelakang + ". umur saya " + umur + " tahun");
+
+// oprator perbandingan
+// let angka1 =10;
+// let angka2 =10;
+
+// let hasil = angka1 == angka2;
+// console.log(hasil);
+
+// let angka1 =10;
+// let angka2 ="10";
+
+// let hasil = angka1 === angka2; //cek nilai dan tipe data
+// console.log(hasil);
+
+//ternary oprator
+// let angka =5;
+// let hasil = angka < 0 ?"negatif" : "positif";
+// console.log(hasil);
